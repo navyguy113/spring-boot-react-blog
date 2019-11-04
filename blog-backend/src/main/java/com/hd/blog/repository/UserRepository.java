4de97d5506252ffc181c60bfe1f258d0680db4c4
navyguy113@gmail.com <navyguy113@gmail.com>
2019-11-04 23:14:13 +0900

    add config & User Service,Repository

---
+++ blog-backend/src/main/java/com/hd/blog/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.hd.blog.repository;
+
+import com.hd.blog.model.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneById(Long id);
+    Optional<User> findByEmail(String email);
+    Optional<User> findOneWithAuthoritiesByEmail(String lowercaseEmail);
+
+    Boolean existsByEmail(String email);
+}
---
+++ blog-backend/src/main/java/com/hd/blog/service/UserService.java
@@ -0,0 +1,25 @@
+package com.hd.blog.service;
+
+import com.hd.blog.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    private final UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //TODO
+}
