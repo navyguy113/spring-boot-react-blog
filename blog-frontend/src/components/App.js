223eeb718316288dbfc866ad2bb6a62a5e89a2e2
dooyeon <endus0713@naver.com>
2019-11-05 23:57:05 +0900

    react 작업중

--- blog-frontend/src/components/App.js
+++ blog-frontend/src/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react'; 
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LoginContainer from 'containers/LoginContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <Route path="/login" component={LoginContainer} />       
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route path="/login" component={LoginContainer} />       
+      </div>
+    );
+  }
+};
 
 export default App;
--- blog-frontend/src/containers/LoginContainer.js
+++ blog-frontend/src/containers/LoginContainer.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
 
-import { Redirect } from 'react-router-dom';
+import React, { Component } from 'react';
 import LoginModal from '../components/Login/LoginModal';
 
 class LoginContainer extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
     
-        this.state = {
-            showModal: false
-        };
-    }
+    //     this.state = {
+    //         showModal: false
+    //     };
+    // }
     
-    handleClose = () => {
-        this.setState(prevState => ({
-            showModal: !prevState.showModal
-        }));
-    };
+    // handleClose = () => {
+    //     this.setState(prevState => ({
+    //         showModal: !prevState.showModal
+    //     }));
+    // };
 
     
     render() {
-        const { showModal } = this.state;
-        const { showModal } = this.state;
-        return (  
-            <LoginModal showModal={!showModal} handleClose={this.handleClose} />
-        );
+        return ( <LoginModal/>);
+        // const { showModal } = this.state;
+        // const { showModal } = this.state;
+        // return (  
+        //     <LoginModal showModal={!showModal} handleClose={this.handleClose} />
+        // );
     }
 
 }
