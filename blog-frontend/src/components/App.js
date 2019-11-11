f8c569700c6a8e54e05332b69fe51c00dd821612
dooyeon <endus0713@naver.com>
2019-11-12 01:59:08 +0900

    login modal

--- blog-frontend/src/components/App.js
+++ blog-frontend/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import LoginContainer from 'containers/LoginContainer'
+import LoginContainer from '../containers/LoginContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/login" component={LoginContainer} />       
+        <LoginContainer/>
+        {/* <Route path="/login" component={LoginContainer} />        */}
       </div>
     );
   }
--- blog-frontend/src/containers/LoginContainer.js
+++ blog-frontend/src/containers/LoginContainer.js
@@ -1,31 +1,52 @@
 
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import LoginModal from '../components/Login/LoginModal';
 
 class LoginContainer extends Component {
-    // constructor(props) {
-    //     super(props);
-    
-    //     this.state = {
-    //         showModal: false
-    //     };
-    // }
-    
-    // handleClose = () => {
-    //     this.setState(prevState => ({
-    //         showModal: !prevState.showModal
-    //     }));
-    // };
 
     
-    render() {
-        return ( <LoginModal/>);
-        // const { showModal } = this.state;
-        // const { showModal } = this.state;
-        // return (  
-        //     <LoginModal showModal={!showModal} handleClose={this.handleClose} />
-        // );
-    }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          showModal: false
+        };
+      }
+    
+      componentDidUpdate(prevProps, prevState) {
+        if (this.props !== prevProps) {
+          this.setState({ showModal: this.props.showModal });
+        }
+      }
+    
+    //   handleLogin = async (username, password, rememberMe = false) => {
+    //     const { AuthActions } = this.props;
+    //     try {
+    //       await AuthActions.login(username, password);      
+    //     } catch (e) {
+    //       console.log("error log :" + e);
+    //     }
+    //   }; 
+    
+      handleClose = () => {
+        this.setState(prevState => ({
+          showModal: !prevState.showModal
+        }));
+      };
+    
+      render() {
+        const { location, isAuthenticated } = this.props;
+        const { showModal } = this.state;
+        // const { from } = location.state || { from: { pathname: '/', search: location.search } };
+    
+        // if (isAuthenticated) {
+        //    return <Redirect to={from} />;
+        // }
+        return (  
+          <LoginModal showModal={!showModal} handleLogin={this.handleLogin} handleClose={this.handleClose} loginError={this.props.loginError} />
+        );
+      }
 
 }
 
