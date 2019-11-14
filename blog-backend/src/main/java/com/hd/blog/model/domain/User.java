c873c78ee47d385940e3cc8834485a040f976c40
navyguy113@gmail.com <navyguy113@gmail.com>
2019-11-14 15:25:43 +0900

    add Repository

--- blog-backend/src/main/java/com/hd/blog/model/domain/User.java
+++ blog-backend/src/main/java/com/hd/blog/model/domain/User.java
@@ -1,11 +1,13 @@
 package com.hd.blog.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hd.blog.model.BaseModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -17,7 +19,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.security.AuthProvider;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -55,20 +59,19 @@ public class User extends BaseModel {
     private String providerId;
 
 
-    //TODO
-//    @JsonIgnore
-//    @ManyToMany
-//    @JoinTable(
-//            name = "user_authority",
-//            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
-//            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")})
 
-//    @BatchSize(size = 20)
-//    private Set<Authority> authorities = new HashSet<>();
+    @BatchSize(size = 20)
+    private Set<Authority> authorities = new HashSet<>();
 
-//    @OneToMany(mappedBy = "user")
-//    @JsonBackReference
-//    private Set<Post> posts;
+    @OneToMany(mappedBy = "user")
+    @JsonBackReference
+    private Set<Post> posts;
 
     @CreatedBy
     @Column(name = "created_by", length = 50, updatable = false)
---
+++ blog-backend/src/main/java/com/hd/blog/repository/PostRepository.java
@@ -0,0 +1,23 @@
+package com.hd.blog.repository;
+
+import com.hd.blog.model.domain.Post;
+import com.hd.blog.model.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByUserOrderByCreatedDateDesc(User user, Pageable pageable);
+
+    Page<Post> findAllByOrderByCreatedDateDesc(Pageable pageable);
+
+    Optional<Post> findById( Long id);
+
+    void delete(Post post);
+}
+
